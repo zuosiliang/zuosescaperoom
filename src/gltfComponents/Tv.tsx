98dea10e20b1b30b81d136f05b1d5a74818c19ab
siliang.zuo <zuosiliang1995@outlook.com>
2024-06-03 13:29:40 +0800

    fix(tv): display video texture in right ratio on tv screen

--- src/gltfComponents/Tv.tsx
+++ src/gltfComponents/Tv.tsx
@@ -3,15 +3,41 @@ import * as THREE from "three";
 import { useGame } from "../store/useGame";
 import { TV_STATE } from "../const";
 import { MODELS } from "../const";
+import { useEffect, useCallback } from "react";
 
 // TODO
+//
+// DONE
 // 修复视频材质没有正好填满屏幕的问题
 function Tv(props) {
   const videoTexture = useVideoTexture("./dogs.mp4");
 
   const { tvState } = useGame();
 
-  const { nodes, materials } = useGLTF("./tv-v3.glb");
+  const { nodes, materials } = useGLTF("./tv-v4.glb");
+
+  const updateScreenUvCoords = useCallback(() => {
+    const uvAttribute = nodes.screen.geometry.attributes.uv;
+
+    uvAttribute.needsUpdate = true;
+    const uvCoordsForVideo = [
+      [0, 1],
+      [1, 1],
+      [0, 0],
+      [1, 0],
+    ];
+
+    for (let i = 0; i < uvAttribute.count; i++) {
+      uvAttribute.setXY(i, uvCoordsForVideo[i][0], uvCoordsForVideo[i][1]);
+    }
+  }, [nodes]);
+
+  useEffect(() => {
+    if (tvState === TV_STATE.OPENED) {
+      updateScreenUvCoords();
+    }
+  }, [tvState, updateScreenUvCoords]);
+
   return (
     <group {...props} dispose={null}>
       <group position={[0.57, 0, 0]}>
@@ -41,6 +67,6 @@ function Tv(props) {
   );
 }
 
-useGLTF.preload("/tv-v3.glb");
+useGLTF.preload("/tv-v4.glb");
 
 export default Tv;
